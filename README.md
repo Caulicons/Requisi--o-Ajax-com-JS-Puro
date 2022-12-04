838dac15759075daa064e1ff732368002bd04e3d
Vitor <caulicons@hotmail.com>
2022-12-04 19:55:00 -0300

    Add a preview

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Learning Ajax with JS, using API Fetch of the JS.
 
 Base project (HTML and CSS) by Matheus Castiglioni, Created for exemplify request Ajax in the post in her [blog](http://blog.matheuscastiglioni.com.br). Cited post: [Preenchendo formulário HTML automaticamente com AJAX](http://blog.matheuscastiglioni.com.br/requisicoes-ajax-com-javascript)
+
+
+## Preview
+![Project Preview](preview.gif)
--- main.js
+++ main.js
@@ -1,50 +1,47 @@
-const inputCep = document.querySelector(`[date-form="cep"]`)
-const btn = document.querySelector(`[date-form="btn"]`)
+const inputCep = document.querySelector(`[date-form="cep"]`);
+const btn = document.querySelector(`[date-form="btn"]`);
 
+/* Mask */
 const masks = {
-     cep (value) {
-        return value
-        .replace(/\D/g, '')
-        .replace(/(\d{5})(\d)/, '$1-$2')
-        .replace(/(-\d{3})\d+?$/, '$1')
-     }
-}
+  cep(value) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{5})(\d)/, "$1-$2")
+      .replace(/(-\d{3})\d+?$/, "$1");
+  },
+};
 
-inputCep.addEventListener('input', (e) => {
-    e.target.value = masks.cep(e.target.value)
-})
+/* handle inputs */
+inputCep.addEventListener("input", (e) => {
+  e.target.value = masks.cep(e.target.value);
+});
 
+/* Handle inputs */
+btn.addEventListener("click", function buscadoCep(e) {
+  e.preventDefault();
 
-btn.addEventListener('click', async function buscadoCep(e) {
-    e.preventDefault()
+  const cep = inputCep.value;
+  buscandoEndereço(cep);
+});
 
-    const cep = inputCep.value
-    const endereço = await buscandoEndereço(cep)
+async function buscandoEndereço(cep) {
+  try {
+    await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.erro) throw new Error();
 
-})
-
-async function buscandoEndereço(cep){
-    try{
-    const busca = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then((res) => res.json())
-    .then((res) => {
-        if (res.erro){
-            inputCep.value = 'CEP Inválido!'
-            inputCep.style.borderColor = "red"
-        } else {
-            document.querySelector('[name="endereco"]').value = res.logradouro
-            document.querySelector('[name="ddd"]').value = res.ddd
-            document.querySelector('[name="bairro"]').value = res.bairro
-            document.querySelector('[name="complemento"]').value = res.complemento
-            document.querySelector('[name="cidade"]').value = res.localidade
-            document.querySelector('[name="estado"]').value = res.uf
-            inputCep.style.borderColor = ""
-        }
-       
-    })
-    return busca
-    } catch(error) {
-        inputCep.value = 'CEP Inválido!'
-        inputCep.style.borderColor = "red"
-    }
+        document.querySelector('[name="endereco"]').value = res.logradouro;
+        document.querySelector('[name="ddd"]').value = res.ddd;
+        document.querySelector('[name="bairro"]').value = res.bairro;
+        document.querySelector('[name="complemento"]').value = res.complemento;
+        document.querySelector('[name="cidade"]').value = res.localidade;
+        document.querySelector('[name="estado"]').value = res.uf;
+        inputCep.style.borderColor = "";
+      });
+  } catch {
+    console.log("passou aqui");
+    inputCep.value = "CEP Inválido!";
+    inputCep.style.borderColor = "red";
+  }
 }
